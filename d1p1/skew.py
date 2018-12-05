45a2ff3d117ddbbcdffb6a64a097bdea0bdfad99
beef salad <beefsalad@beefsalad.com>
2018-12-05 13:08:56 -0600

    fixed gitignore added d1p2

--- d1p1/skew.py
+++ d1p1/skew.py
@@ -1,4 +1,4 @@
-#    # #!/usr/bin/python3.7
+#!/usr/bin/python3.7
 # calculates freq skew.
 import os
 # shit portability, on the to learn list
@@ -7,11 +7,7 @@ print(path)
 freq = 0
 change = 0
 with open(path) as filepath:
-        for change in filepath.readlines():
-            change = int(change) #hmmm should be a way to readlines as ints?
-            freq += change
+    for change in filepath.readlines():
+        change = int(change)  # hmmm should be a way to readlines as ints?
+        freq += change
 print(freq)
-
-
-
-

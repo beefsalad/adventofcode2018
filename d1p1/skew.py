b68eeef5492e9614e0760bac4e7b3146578785b5
beef salad <beefsalad@beefsalad.com>
2018-12-05 12:51:01 -0600

    aoc2018 day 1 part 1 it is not pretty but it works

---
+++ d1p1/skew.py
@@ -0,0 +1,17 @@
+#    # #!/usr/bin/python3.7
+# calculates freq skew.
+import os
+# shit portability, on the to learn list
+path = os.getcwd()+"/input.txt"
+print(path)
+freq = 0
+change = 0
+with open(path) as filepath:
+        for change in filepath.readlines():
+            change = int(change) #hmmm should be a way to readlines as ints?
+            freq += change
+print(freq)
+
+
+
+

dad965ab46b2838b5172ac2084134db7c7c8187e
beef salad <beefsalad@beefsalad.com>
2018-12-17 00:20:16 -0600

    attempt #2, still shit

--- d1p2/findrepeatfrequency.py
+++ d1p2/findrepeatfrequency.py
@@ -3,40 +3,50 @@
 
 import os
 
-# set globals
-change = None
-changelist = []
-n = 0
-
 # frequency starts at 0...stop removing this!
 freq = 0
-# frequency starts at 0...but if we instantiate now the first check will
-# return a false positive!
-freqlist = []
+n = 0 
+
+# frequency starts at 0...but if we instantiate now will the first check work? nope!
+changelist = []
 # shit portability, still on the to learn list
 path = os.getcwd()+"/input.txt"
 with open(path) as filepath:
     for change in filepath.readlines():
+        # hmmmm, do I have to do create changelist from change or can I just append change.....so much lost
+        # I don't remember what didn't like change not being int()ed, should research why
         change = int(change)
         changelist.append(change)
-
-# print(changelist)
-# print(freq)
-
-
-def check(freq, n):
-    freqcount = len(freqlist)
-    changecount = len(changelist)
-    # print(freq, freqlist, n, len(freqlist))
-    while freq not in freqlist and n < freqcount:
-        freq = changelist[n]
-        freqlist.append(freq)
-        print(freq, freqlist)
-        n += 1
-        print(n)
-        if n == changecount:
-            n = 0
-            return(n, freq)
+changecount = len(changelist)
+#print(changelist)
+#print(changecount)
+#print(freq)
 
 
-check(freq, n)
+#check freq in freqlist
+# if freq not in freqlist
+# change frequency
+#   the abstraction here is killing me, as soon as I append to the frequency list the conditional is met?
+
+def check (foundfreq, changes, changecount):
+    n = 0
+    freqlist = []
+    while n < changecount:
+        foundfreq += changes[n]
+        if foundfreq in freqlist:
+            print('found dupe?!', foundfreq)
+        else:
+            freqlist.append(foundfreq)
+            n += 1
+    else:
+        n = 0
+        print(n, 'try again!')   
+        freqlist.append(foundfreq)
+        return (freq)
+
+
+
+check(freq, changelist, len(changelist))
+
+print('the answer is not 0 and its not 13 or -12, or 576i think its ', freq)
+print('and we still arent checking for duplicates...just checking if in the list.  need to seperate it out further somehow...if freq = freqlist(n-1)? hrmph')
